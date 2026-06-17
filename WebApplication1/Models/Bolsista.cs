51dca2de7434cafb3b8a30bf708d14102ccedc9f
Estagio Acb <Estagio Acb@LABRA01>
2026-06-17 15:53:49 -0300

    semana1_codigo_inicial_vitor

--- WebApplication1/Models/Bolsista.cs
+++ WebApplication1/Models/Bolsista.cs
@@ -11,16 +11,24 @@ namespace WebApplication1.Models
         public string Nome { get; set; }
 
         // TODO: Criar a propriedade para o CPF
-
+        public string CPF { get; set; }
         // TODO: Criar a propriedade para a Matrícula
-
+        public string matricula { get; set; }
         // TODO: Criar a propriedade para a Data de Nascimento
-
+        public DateTime data_de_nascimento { get; set; }
         // TODO: Criar a propriedade para o Sexo
-
+        public char sexo { get; set; }
         // TODO: Criar método com o resumo das informações contendo nome e matrícula
+        public string resumir_usuario()
+        {
+            return $"bolsista: {Nome} matricula: {matricula}";
 
+        }
         //TODO: Criar método que calcúla a idade do bolsista      
-
+        public int calcular_idade ()
+        {
+            int idade = DateTime.Now.Year - data_de_nascimento.Year;
+            return idade;
+        }
     }
 }
